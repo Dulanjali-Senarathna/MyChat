5255d86315c81a756e5d89941259fe82798060b1
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-08-03 11:10:06 +0530

    Designing Login Page Done

--- signin.php
+++ signin.php
@@ -3,13 +3,12 @@
 <html>
 <head>
 	<title>Login to your account</title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="https://fonts.googleapis.com/css?family=Roboto|Courgette|Pacifico:400,700" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/signin.css">
 </head>
 <body>
 	<div class="signin-form">
@@ -32,8 +31,8 @@
 				</button>
 			</div>	
 		</form>
-		<div class="text-center small" style="color: #674288;">
-			Don't have an account? <a href="signup.php">Create one</a>
+		<div class="text-center small" style="color: #ffffff;">
+			Don't have an account? <a href="signup.php" style="color:#ffffff">Create one</a>
 		</div>
 	</div>
 </body>
